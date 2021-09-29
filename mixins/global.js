29b861697dc4d99f4542a55e7680cbbe70db44d5
Misti <mistier.famous@gmail.com>
2021-09-29 20:30:09 +0600

    123

--- mixins/global.js
+++ mixins/global.js
@@ -23,6 +23,7 @@ const globals = {
             }
           },
           getEventum(id) {
+
             return this.$store.state.eventum.code + id
           },
           getStarClass(item, max) {
